372bab38c134551094fcfc897c8bac739a6ff9f4
Kurban <kurbanovkk96@mail.ru>
2022-07-07 13:02:37 +0300

    div2

--- Example004_div/Program.cs
+++ Example004_div/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double number1 = 11;
+double number2 = 5;
+double result = number1 / number2;
+System.Console.WriteLine(result);
