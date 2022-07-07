50e9100d90366978ce956b8994e4d9dd8634e098
Kurban <kurbanovkk96@mail.ru>
2022-07-07 12:46:25 +0300

    Sum

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,6 @@
+﻿
+System.Console.WriteLine("Для нахождения суммы чисел необходимо определить числа, число №1: ");
+int number1 = int.Parse(Console.ReadLine());
+System.Console.WriteLine("Введите число №2: ");
+int number2 = int.Parse(Console.ReadLine());
+System.Console.WriteLine(number1 + number2);
