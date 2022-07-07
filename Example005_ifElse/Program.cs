bd42df66358c89f557cb6bdca321adfc60f68404
Kurban <kurbanovkk96@mail.ru>
2022-07-07 13:19:30 +0300

    ifElse

---
+++ Example005_ifElse/Program.cs
@@ -0,0 +1,12 @@
+﻿System.Console.Write("ВВедите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")  // ToLower аозволят понимать внезависимости от то заглавная буква или обычная
+{
+    System.Console.WriteLine("Ура, это же Маша!!!");
+}
+else
+{
+    System.Console.WriteLine($"привет " + username);
+
+}
