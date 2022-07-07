98ca1ace3a0c22ac81a0ef4cc69ffb38306454f6
Kurban <kurbanovkk96@mail.ru>
2022-07-07 13:39:47 +0300

    maxmin

---
+++ Example006_Basic/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+int d = int.Parse(Console.ReadLine());
+int e = int.Parse(Console.ReadLine());
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+System.Console.Write($"Максимальное число равно: " + max);
